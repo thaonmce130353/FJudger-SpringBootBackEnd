6f03c5adb1abe993c8b5b392a3ab3294a3ea2286
thaonmce130353 <nguyenminhthaobt1@gmail.com>
2021-10-12 12:09:59 +0700

    add controller and repository package

---
+++ src/main/java/study/springboot/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package study.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import study.springboot.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long>{
+	
+}
